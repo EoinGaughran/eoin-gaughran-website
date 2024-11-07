8fbc83d873b93b3b09ebb4de748dd6cabd329e70
Eoin Gaughran <eoinbgaughran@gmail.com>
2024-11-07 14:50:38 +0100

    image detection example

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs'); // You can use a different template engine if you prefer
+
+app.get('/skibidi', (req, res) => {
+  res.render('pages/opencv')
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.render('pages/index')
 });
 
 app.listen(PORT, () => {
